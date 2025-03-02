eebe954eae7ff782f6afbcf781ee8a66f159b52b
rupesh-kaneki88 <rupeshchavan048@gmail.com>
2025-03-02 18:26:13 +0530

    pre prod - 01 .env issue

--- src/app/api/create-checkout-session/route.js
+++ src/app/api/create-checkout-session/route.js
@@ -2,6 +2,10 @@
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("STRIPE_SECRET_KEY is missing");
+}
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request) {
